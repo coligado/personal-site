263eb133e11f30f155340d59f5bc1b8484f2ae64
Lea Coligado <lcoligado@imac36s-iMac.local>
2014-08-28 14:42:10 -0700

    Added all my website code to the git repo

---
+++ js/templatemo_custom.js
@@ -0,0 +1,18 @@
+/* Credit: http://www.templatemo.com */
+
+$(document).ready(function() {
+	$('#services .services_buttons li').each( function(){
+		$(this).on('click', function(){
+			change_panels( $(this).index() );
+		});
+	});
+});
+
+function change_panels(new_index){
+	var arrow_positions = [ 20, 110, 205 ];
+	var new_top = arrow_positions[new_index] + 'px';
+	$('.arrow-left').animate({marginTop:new_top}, 500);
+	$('#services_tabs li:nth-child('+(new_index+1)+')').tab('show');
+	$('.services_buttons li').removeClass('active');
+	$('.services_buttons li:nth-child('+(new_index+1)+')').addClass('active');
+}
